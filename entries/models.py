c593e183d1a0b1e7f2c782d10542756075ebdb96
javitocor <javitocor@gmail.com>
2021-09-08 22:56:37 +0700

    initil commit

---
+++ entries/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Entry(models.Model):
+  text = models.TextField()
+  date_posted = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return 'Entry #{}'.format(self.id)
+
+  class Meta: 
+    verbose_name_plural = 'entries'
